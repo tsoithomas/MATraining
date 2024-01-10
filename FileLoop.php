aefefdfa7b882b8debb9f1732d72545afa73e41f
Thomas Tsoi <tsoithomas@outlook.com>
2024-01-10 10:29:53 -0500

    created initial logic

--- FileLoop.php
+++ FileLoop.php
@@ -1,4 +1,27 @@
 <?php
 
+if ($argc != 2)
+	die("Incorrect number of arguments.");
+
+$path = $argv[1];
+
+$fh = fopen($path, "r") or die("Unable to open file!");
 
 
+$capcount = 0;
+while(!feof($fh)) {
+	$data = fread($fh, 8192);
+	for ($i=0; $i<strlen($data); $i++) {
+		$ch = $data[$i];
+		if ($ch == "\n") {
+			echo "$capcount\n";
+			$capcount = 0;
+		}
+		elseif (mb_strtolower($ch) != $ch)
+			$capcount++;
+	}
+	//echo $data;
+}
+
+
+fclose($fh);
